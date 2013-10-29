5938dd599bb736576417baeb9e4ab3ff2c9e8086
Monica Toth <mh.toth@gmail.com>
2013-10-29 17:07:22 -0400

    Greatly simplified schema and added revisioning function + trigger

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -33,19 +33,16 @@ create table elements (
 	created timestamp not null default now(),
 	space_id REFERENCES spaces ON DELETE CASCADE,
 	element_type REFERENCES element_types ON DELETE RESTRICT,
-	title varchar(100) not null,
+	title varchar(100) default 'pearl_element',
 	-- latest REFERENCES element_revisions	-- table not created yet
 );
 
--- TOCs only apply to page elements 
 create table toc (
 	id serial primary key,
 	created timestamp not null default now(),
 	element_id REFERENCES elements ON DELETE CASCADE,
-	element_type REFERENCES elements(element_type),
-	order integer not null,
-	name varchar(500),
-	CONSTRAINT must_be_page CHECK (element_type = 1)
+	ordinal integer default 0,
+	name varchar(500)
 );
 
 -- There are currently 8 context types available
@@ -57,20 +54,48 @@ create table context_types (
 
 INSERT INTO context_types (id, ordinal, type_name) VALUES
 	(1, 0, 'main'),
-	(2, 1, 'abstract'),
-	(3, 2, 'summary'),
-	(4, 3, 'basics'),
-	(5, 4, 'motivation'),
+	(2, 1, 'subtitle'),
+	(3, 2, 'abstract'),
+	(4, 3, 'motivation'),
+	(5, 4, 'basics'),
 	(6, 5, 'opinions'),
 	(7, 6, 'social'),
 	(8, 7, 'xcredit');
 
--- There are contexts for most TOC elements in a page
 create table contexts (
 	id serial primary key,
 	created timestamp not null default now(),
 	toc_id REFERENCES toc ON DELETE CASCADE,
-	context_type REFERENCES context_types ON DELETE RESTRICT,
-	CONSTRAINT appropriate_context_type 
-	CHECK (context_type = 1 OR context_type > 3)
+	context_type REFERENCES context_types ON DELETE RESTRICT
+	content text
 );
+
+create table revisions (
+	id serial primary key,
+	created timestamp not null default now(),
+	context_id REFERENCES(contexts),
+	action varchar(100),
+	content text
+);
+
+CREATE OR REPLACE FUNCTION revisions_function() RETURNS trigger AS '
+BEGIN
+	IF tg_op = ''DELETE'' THEN
+		INSERT INTO revisions (context_id, content, action) VALUES (old.context_id, old.content, ''DELETE'');
+		RETURN old;
+	END IF;
+	IF tg_op = ''INSERT'' THEN
+		INSERT INTO revisions (context_id, content, action) VALUES (new.context_id, new.content, ''INSERT'');
+		RETURN new;
+	END IF;
+	IF tg_op = ''UPDATE'' THEN
+		INSERT INTO revisions (context_id, content, action) VALUES (old.context_id, old.content, ''UPDATE'');
+		RETURN new;
+	END IF;
+END
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER revisions_trigger
+AFTER INSERT OR DELETE OR UPDATE ON contexts
+FOR each ROW EXECUTE PROCEDURE
+	revisions_function();
