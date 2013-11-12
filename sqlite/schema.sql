873a6807d0737b4bd04e8ff8b8cbc7b673ba0866
Monica Toth <mh.toth@gmail.com>
2013-11-12 11:03:33 -0500

    Finally removing old SQLite folder

--- sqlite/schema.sql
+++
@@ -1,8 +0,0 @@
-/* each space is a distinct wiki document */
-drop table if exists spaces;
-create table spaces (
-	id integer primary key autoincrement,
-	created timestamp default (strftime('%s', 'now')),
-	title text not null
-);
-
